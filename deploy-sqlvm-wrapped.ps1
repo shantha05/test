2450591dfd387affffe660a39cea9ce0a7c56474
shantha05 <shantha05@gmail.com>
2022-04-18 13:45:47 +0530

    Update deploy-sqlvm-wrapped.ps1

--- deploy-sqlvm-wrapped.ps1
+++ deploy-sqlvm-wrapped.ps1
@@ -38,7 +38,7 @@ $backups = "F:\Backup"
 Write-Output "Set up data, backup and log directories in SQL, plus mixed-mode auth"
 Import-Module "sqlps" -DisableNameChecking
 [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
-$sqlesq = new-object ('Microsoft.SqlServer.Management.Smo.Server') 10.0.2.4
+$sqlesq = new-object ('Microsoft.SqlServer.Management.Smo.Server') .
 $sqlesq.Settings.LoginMode = [Microsoft.SqlServer.Management.Smo.ServerLoginMode]::Mixed
 $sqlesq.Settings.DefaultFile = $data
 $sqlesq.Settings.DefaultLog = $logs
